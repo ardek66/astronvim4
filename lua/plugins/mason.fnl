2b1706d10a13805129cd8430018b88af1d06cf44
IDF <ardek66@tutanota.com>
2023-12-31 02:46:13 +0200

    Convert mason to fennel

---
+++ lua/plugins/mason.fnl
@@ -0,0 +1,19 @@
+(local astrocore (require :astrocore))
+[{ 1 :williamboman/mason-lspconfig.nvim
+  :opts (fn [_ opts]
+          (local overrideServers [ "clangd" ])
+
+          (set opts.ensure_installed 
+               (astrocore.list_insert_unique 
+                 opts.ensure_installed
+                 [ "fennel_language_server" ]))
+
+          (let 
+            [rev (collect 
+                   [k v (ipairs opts.ensure_installed)] 
+                   (values v k))] 
+            (each [_ blocked (ipairs overrideServers)] 
+              (table.remove opts.ensure_installed (. rev blocked)))))}
+
+  { 1 :jay-babu/mason-null-ls.nvim }
+  { 1 :jay-babu/mason-nvim-dap.nvim }]
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,48 +1,23 @@
--- Example customization of mason plugins
-return {
-  -- use mason-lspconfig to configure LSP installations
-  {
-    "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
-    opts = function(_, opts)
-      local overrideServers = { "clangd" }
-
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "fennel_language_server",
-        -- "lua_ls",
-      })
-
-      local revMap = {}
-      for k, v in ipairs(opts.ensure_installed) do
-        revMap[v] = k
+-- [nfnl] Compiled from lua/plugins/mason.new.fnl by https://github.com/Olical/nfnl, do not edit.
+local astrocore = require("astrocore")
+local function _1_(_, opts)
+  local overrideServers = {"clangd"}
+  opts.ensure_installed = astrocore.list_insert_unique(opts.ensure_installed, {"fennel_language_server"})
+  local rev
+  do
+    local tbl_14_auto = {}
+    for k, v in ipairs(opts.ensure_installed) do
+      local k_15_auto, v_16_auto = v, k
+      if ((k_15_auto ~= nil) and (v_16_auto ~= nil)) then
+        tbl_14_auto[k_15_auto] = v_16_auto
+      else
       end
-
-      for _, v in ipairs(overrideServers) do
-        table.remove(opts.ensure_installed, revMap[v])
-      end
-    end,
-  },
-  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-  {
-    "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
-      })
-    end,
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        -- "python",
-      })
-    end,
-  },
-}
+    end
+    rev = tbl_14_auto
+  end
+  for _0, blocked in ipairs(overrideServers) do
+    table.remove(opts.ensure_installed, rev[blocked])
+  end
+  return nil
+end
+return {{"williamboman/mason-lspconfig.nvim", opts = _1_}, {"jay-babu/mason-null-ls.nvim"}, {"jay-babu/mason-nvim-dap.nvim"}}
