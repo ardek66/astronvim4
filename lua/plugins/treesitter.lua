a2074d7510733fd5010fc0a3a090c57a2b4306ca
IDF <ardek66@tutanota.com>
2024-01-24 04:56:18 +0200

    Fix treesitter.fnl for v4 changes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 -- [nfnl] Compiled from lua/plugins/treesitter.fnl by https://github.com/Olical/nfnl, do not edit.
 local astrocore = require("astrocore")
 local function _1_(_, opts)
-  opts.ensure_installed = astrocore.list_insert_unique(opts.ensure_installed, {"rust", "nim", "nim_format_string", "fennel"})
+  opts.ensure_installed = astrocore.list_insert_unique(opts.ensure_installed, "rust", "nim", "nim_format_string", "fennel")
   return opts
 end
 return {"nvim-treesitter/nvim-treesitter", opts = _1_}
